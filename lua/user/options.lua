50bfff7b0b04139321b3eeb64aff10a9943263a1
Rafael Muijsert <rmuijsert@gmail.com>
2023-01-16 23:31:02 +0100

    initial commit

---
+++ lua/user/options.lua
@@ -0,0 +1,11 @@
+local options = {
+	undofile = true,	-- Persistent undo
+	expandtab = false,	-- Do not convert tabs to spaces
+	shiftwidth = 4,		-- Insert 4 spaces for each indentation
+	tabstop = 4,		-- Insert 4 spaces for a tab
+}
+
+for key, value in pairs(options) do
+	vim.opt[key] = value
+end
+
